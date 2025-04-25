cfb2f557385a68f2b7d2dfe1eff40ab0d62b0479
absorfy <mr.laggermpc.890@gmail.com>
2025-04-25 16:10:51 +0200

    setup react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const selectedGame = 'bejeweled';
+
+  return (
+    <div className="App">
+      <h1>GameStudio</h1>
+      Selected game: {selectedGame}
+    </div>
+  );
+}
+
+export default App;
